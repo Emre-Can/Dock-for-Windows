287d76c9e9a9e29ded1178bb225cb08b65dbf11e
Emre Can ÇİMKE <emrecancimke@gmail.com>
2022-06-28 00:00:03 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace UygulamaBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonDiger2 = new System.Windows.Forms.Button();
+            this.buttonDiger1 = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonGmail = new System.Windows.Forms.Button();
+            this.buttonBupc = new System.Windows.Forms.Button();
+            this.buttonChrome = new System.Windows.Forms.Button();
+            this.elipseControl6 = new UygulamaBar.ElipseControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1213, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kapat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(1213, 78);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ayarla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonDiger2
+            // 
+            this.buttonDiger2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDiger2.FlatAppearance.BorderSize = 0;
+            this.buttonDiger2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDiger2.Location = new System.Drawing.Point(560, 5);
+            this.buttonDiger2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonDiger2.Name = "buttonDiger2";
+            this.buttonDiger2.Size = new System.Drawing.Size(133, 123);
+            this.buttonDiger2.TabIndex = 10;
+            this.buttonDiger2.UseVisualStyleBackColor = true;
+            this.buttonDiger2.Visible = false;
+            this.buttonDiger2.Click += new System.EventHandler(this.buttonDiger2_Click);
+            // 
+            // buttonDiger1
+            // 
+            this.buttonDiger1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDiger1.FlatAppearance.BorderSize = 0;
+            this.buttonDiger1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDiger1.Location = new System.Drawing.Point(421, 5);
+            this.buttonDiger1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonDiger1.Name = "buttonDiger1";
+            this.buttonDiger1.Size = new System.Drawing.Size(133, 123);
+            this.buttonDiger1.TabIndex = 8;
+            this.buttonDiger1.UseVisualStyleBackColor = true;
+            this.buttonDiger1.Visible = false;
+            this.buttonDiger1.Click += new System.EventHandler(this.buttonDiger1_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackgroundImage = global::UygulamaBar.Properties.Resources.icons8_shutdown_100px;
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Location = new System.Drawing.Point(1072, 5);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(133, 123);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonGmail
+            // 
+            this.buttonGmail.BackgroundImage = global::UygulamaBar.Properties.Resources.icons8_gmail_100px;
+            this.buttonGmail.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonGmail.FlatAppearance.BorderSize = 0;
+            this.buttonGmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGmail.Location = new System.Drawing.Point(280, 5);
+            this.buttonGmail.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonGmail.Name = "buttonGmail";
+            this.buttonGmail.Size = new System.Drawing.Size(133, 123);
+            this.buttonGmail.TabIndex = 5;
+            this.buttonGmail.UseVisualStyleBackColor = true;
+            this.buttonGmail.Click += new System.EventHandler(this.buttonGmail_Click);
+            // 
+            // buttonBupc
+            // 
+            this.buttonBupc.BackgroundImage = global::UygulamaBar.Properties.Resources.icons8_windows_client_100px;
+            this.buttonBupc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonBupc.FlatAppearance.BorderSize = 0;
+            this.buttonBupc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBupc.Location = new System.Drawing.Point(-5, -5);
+            this.buttonBupc.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonBupc.Name = "buttonBupc";
+            this.buttonBupc.Size = new System.Drawing.Size(133, 123);
+            this.buttonBupc.TabIndex = 4;
+            this.buttonBupc.UseVisualStyleBackColor = true;
+            this.buttonBupc.Click += new System.EventHandler(this.buttonBupc_Click);
+            // 
+            // buttonChrome
+            // 
+            this.buttonChrome.BackgroundImage = global::UygulamaBar.Properties.Resources.icons8_chrome_100px;
+            this.buttonChrome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonChrome.FlatAppearance.BorderSize = 0;
+            this.buttonChrome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonChrome.Location = new System.Drawing.Point(144, 5);
+            this.buttonChrome.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.buttonChrome.Name = "buttonChrome";
+            this.buttonChrome.Size = new System.Drawing.Size(133, 123);
+            this.buttonChrome.TabIndex = 3;
+            this.buttonChrome.UseVisualStyleBackColor = true;
+            this.buttonChrome.Click += new System.EventHandler(this.buttonChrome_Click);
+            // 
+            // elipseControl6
+            // 
+            this.elipseControl6.CornerRadius = 7;
+            this.elipseControl6.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1280, 133);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDiger2);
+            this.Controls.Add(this.buttonDiger1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonGmail);
+            this.Controls.Add(this.buttonBupc);
+            this.Controls.Add(this.buttonChrome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonChrome;
+        private System.Windows.Forms.Button buttonBupc;
+        private System.Windows.Forms.Button buttonGmail;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDiger1;
+        private System.Windows.Forms.Timer timer1;
+        private ElipseControl elipseControl6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonDiger2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using UygulamaBar.Properties;
+using System.Windows.Automation;
+
+namespace UygulamaBar
+{
+    public partial class Form1 : Form
+    {
+        string ProgramAdi = "UygulamaBar";
+        string Diger1 = "";
+        string Diger2 = "";
+        string Diger1kontrol = "";
+        string Diger2kontrol = "";
+        string visualYol = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\devenv.exe";
+        public Form1()
+        {
+            InitializeComponent();
+            //this.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            Taskbar.Gizle();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue(ProgramAdi, "\"" + Application.ExecutablePath + "\"");
+            int ekran_x= Screen.GetBounds(new Point(0, 0)).Width;
+            int ekran_y = Screen.GetBounds(new Point(0, 0)).Height;
+            this.Height = ((ekran_y / 100) * 8);
+            this.Width = ((ekran_x / 100) * 50);
+            this.Location = new Point(((ekran_x / 2) - (this.Width / 2)),(ekran_y - this.Height));
+            buttonBupc.Height = this.Height +10;
+            buttonBupc.Width = buttonBupc.Height;
+            buttonChrome.Height = buttonBupc.Height;
+            buttonChrome.Width = buttonBupc.Width;
+            buttonChrome.Location = new Point(buttonBupc.Width + 8,buttonBupc.Location.Y);
+            buttonGmail.Height = buttonBupc.Height;
+            buttonGmail.Width = buttonBupc.Width;
+            buttonGmail.Location = new Point(buttonBupc.Width + buttonChrome.Width + 12, buttonBupc.Location.Y);
+            buttonClose.Height = buttonBupc.Height;
+            buttonClose.Width = buttonBupc.Width;
+            buttonClose.Location = new Point((this.Width-(buttonClose.Width+button1.Width+8)),buttonBupc.Location.Y);
+            buttonDiger1.Height = buttonBupc.Height;
+            buttonDiger1.Width = buttonBupc.Width;
+            buttonDiger1.Location = new Point(buttonBupc.Width + buttonChrome.Width + buttonGmail.Width+16, buttonBupc.Location.Y);
+            buttonDiger2.Height = buttonBupc.Height;
+            buttonDiger2.Width = buttonBupc.Width;
+            buttonDiger2.Location = new Point(buttonBupc.Width + buttonChrome.Width + buttonGmail.Width +buttonDiger1.Width+ 20, buttonBupc.Location.Y);
+
+
+        }
+        private void buttonBupc_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"explorer.exe");
+        }
+        
+        private void buttonChrome_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.google.com.tr/");
+
+        }
+        
+        private void buttonGmail_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://mail.google.com/mail/u/0/");
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Taskbar.Goster();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.DeleteValue(ProgramAdi);
+            Application.Exit();
+        }
+        private void buttonDiger1_Click(object sender, EventArgs e)
+        {
+            if (Diger1kontrol == "visual")
+                visual();
+            else if (Diger1kontrol == "epicgames")
+                epicgames();
+        }
+        private void buttonDiger2_Click(object sender, EventArgs e)
+        {
+            if (Diger2kontrol == "visual")
+                visual();
+            else if (Diger2kontrol == "epicgames")
+                epicgames();
+        }
+
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(int hwnd);
+
+        public void visual()
+        {
+            //uygulama ekrandayken (simge durumunda değilken) uygulamaya odaklama
+            Process bProcess = Process.GetProcessesByName("devenv").FirstOrDefault();
+            //bProcess. = ProcessWindowStyle.Maximized;
+            ShowWindow(bProcess.Handle, ShowWindowEnum.Restore);
+            AutomationElement element = AutomationElement.FromHandle(bProcess.MainWindowHandle);
+                element.SetFocus();
+                
+            
+        }
+        public void epicgames()
+        {
+            Process.Start("C:\\Program Files (x86)\\Epic Games\\Launcher\\Portal\\Binaries\\Win32\\EpicGamesLauncher.exe");
+        }
+
+        public class Taskbar
+        {
+            [DllImport("user32.dll")]
+            private static extern int FindWindow(string className, string windowText);
+
+            [DllImport("user32.dll")]
+            private static extern int ShowWindow(int hwnd, int command);
+
+            [DllImport("user32.dll")]
+            public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+            [DllImport("user32.dll")]
+            private static extern int GetDesktopWindow();
+
+            private const int SW_HIDE = 0;
+            private const int SW_SHOW = 1;
+
+            protected static int Handle
+            {
+                get
+                {
+                    return FindWindow("Shell_TrayWnd", "");
+                }
+            }
+
+            protected static int HandleOfStartButton
+            {
+                get
+                {
+                    int handleOfDesktop = GetDesktopWindow();
+                    int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                    return handleOfStartButton;
+                }
+            }
+
+            public static void Goster()
+            {
+                ShowWindow(Handle, SW_SHOW);
+                ShowWindow(HandleOfStartButton, SW_SHOW);
+            }
+
+            public static void Gizle()
+            {
+                ShowWindow(Handle, SW_HIDE);
+                ShowWindow(HandleOfStartButton, SW_HIDE);
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Taskbar.Goster();
+            System.Diagnostics.Process.Start("C:\\Windows\\System32\\shutdown.exe");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            Process[] Memory = Process.GetProcesses();
+
+            foreach (Process prc in Memory)
+            {
+                //listBox1.Items.Add(prc.ProcessName);
+                if (prc.ProcessName == "devenv")
+                {
+                    
+                    if (Diger1=="")
+                    {
+                        if (Diger2 == "visual") buttonDiger2.Hide();
+                        buttonDiger1.Show();
+                        buttonDiger1.BackgroundImage = Resources.icons8_visual_studio_64px;
+                        Diger1 = "visual";
+                    }
+                    else if (Diger2 == "")
+                    {
+                        buttonDiger2.Show();
+                        buttonDiger2.BackgroundImage = Resources.icons8_visual_studio_64px;
+                        Diger2 = "visual";
+                    }
+
+                }
+                else if (prc.ProcessName== "EpicGamesLauncher")
+                {
+                    if (Diger1=="")
+                    {
+                        if (Diger2 == "epicgames") buttonDiger2.Hide();
+                        buttonDiger1.Show();
+                        buttonDiger1.BackgroundImage = Resources._31a462f5c4e0136a296cdfe4cafcd275_removebg_preview;
+                        Diger1 = "epicgames";
+                    }
+                    else if (Diger2 == "")
+                    {
+                        buttonDiger2.Show();
+                        buttonDiger2.BackgroundImage = Resources._31a462f5c4e0136a296cdfe4cafcd275_removebg_preview;
+                        Diger2 = "epicgames";
+                    }
+                }
+
+            }
+            if (Diger1 != "visual" && Diger1 != "epicgames") 
+            {
+                buttonDiger1.Hide(); 
+            }
+            if (Diger2 != "visual" && Diger2 != "epicgames")
+            {
+                buttonDiger2.Hide();
+            }
+            Diger1kontrol = Diger1;
+            Diger2kontrol = Diger2;
+            Diger1 = "";
+            Diger2 = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 ayar = new Form2();
+            ayar.Show();
+            ayar.Location = new Point(this.Location.X + button2.Left, this.Location.Y - ayar.Height);
+        }
+    }
+}
